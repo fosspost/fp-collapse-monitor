ed0c78d1d36643808d09355bab1dfaae4a3f4e48
Muhammed Hanny Sabbagh <mhsabbagh@outlook.com>
2022-07-20 23:55:03 +0300

    New attempt to build snap

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
-data_files = [ ("/usr/share/fp-collapse-monitor", ["ui/ui.glade"]),
-                    ("/usr/share/pixmaps", ["data/fp-collapse-monitor.png"]),
-                     ("/usr/share/applications", ["data/fp-collapse-monitor.desktop"]) ] 
-
+data_files = [ ("/usr/share/fp-collapse-monitor", ["ui.glade"]),
+                    ("/usr/share/pixmaps", ["fp-collapse-monitor.png"]),
+                     ("/usr/share/applications", ["fp-collapse-monitor.desktop"]) ] 
+                     
 setup(name = "fp-collapse-monitor",
       version = "1.0",
       description = "Simple out-of-the-box browser for collapse maps worldwide.",
@@ -11,7 +11,7 @@ setup(name = "fp-collapse-monitor",
       author_email = "email@mhsabbagh.com",
       url = "https://github.com/fosspost/fp-collapse-monitor",
       license='GPLv3',
+      py_modules=[],
       scripts=['fp-collapse-monitor'],
-      data_files=data_files,
-      include_package_data=True)
+      data_files=data_files)
 
